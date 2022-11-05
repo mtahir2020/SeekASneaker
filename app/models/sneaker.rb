9615dea6d82401579c2419a1b88e6435ef74d3f3
Mustafa Tahir <mustafatahir@hotmail.co.uk>
2022-11-05 12:59:55 +0000

    index styling

--- app/models/sneaker.rb
+++ app/models/sneaker.rb
@@ -1,4 +1,7 @@
 class Sneaker < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
+  validates :name, presence: true
+  validates :description, length: {minimum: 5, maximum: 25}
+  validates :photo, presence: true
 end
