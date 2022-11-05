f0b4bcecb1b9830bea9fbc9d60e407bfa64b6a26
angelee otic <otic.angeleemae@gmail.com>
2022-11-05 11:52:14 +0000

    removed the 2nd active storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_04_130343) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_03_203204) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
