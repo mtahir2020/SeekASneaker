02b5dace38321fcab152268322f0afa5aca70273
Liubov Koval <lubawakova90@gmail.com>
2022-11-08 21:52:28 +0000

    add sneakers list

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,30 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def my_sneakers
+    @sneakers = Sneaker.where(user: current_user)
+  end
+
+  # def edit
+  #  @sneaker = Sneaker.find(params[:id])
+  # end
+
+  # def update
+  #   @sneaker = Sneaker.find(params[:id])
+  #   @sneaker.update(sneaker_params)
+  #   redirect_to sneaker_path(@sneaker)
+  # end
+
+  # def destroy
+  #   @sneaker = Sneaker.find(params[:id])
+  #   @sneaker.destroy
+  #   redirect_to sneakers_path, status: :see_other
+  # end
+
+  # private
+  # def sneaker_params
+  #   params.require(:sneaker).permit(:user_id, :name, :price, :description. :image_url)
+  # end
+
 end
