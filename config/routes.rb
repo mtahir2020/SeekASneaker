700b722b996aa8b6c8e9148fd31552fbb1a5ae2b
Liubov Koval <lubawakova90@gmail.com>
2022-11-12 12:47:15 +0000

    fix my sneakers list style

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   root to: "sneakers#index"
   get "my_sneakers", to: "pages#my_sneakers"
   resources :sneakers, only: %i[index show create new destroy edit update] do
